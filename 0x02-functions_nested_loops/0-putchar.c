7bef62bee9bc8ed09e9c5d20e11cca41ffefecee
Jaypor <yimikajoshua@gmail.com>
2022-09-13 06:48:08 -0700

    i think its fixed

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,16 +1,16 @@
 /*
  * File: 0-putchar.c
  * Auth: Jaypor
+ *
  */
 
 #include "main.h"
 
 /**
- * main - Prints _putchar followed by a new line.
+ * main - Prints "_putchar" followed by a new line.
  *
  * Return: Always 0.
  */
-
 int main(void)
 {
 	_putchar('_');
